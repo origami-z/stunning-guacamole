66567c67b56d61fc3a009a245723f8e0ba89f7da
origami-z <davidcui2@me.com>
2023-03-06 11:55:16 +0000

    Different HTML title local vs prod

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,25 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
+import { loadEnv } from "vite";
+import { createHtmlPlugin } from "vite-plugin-html";
+
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: "/stunning-guacamole/",
-});
+export default ({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+
+  return defineConfig({
+    plugins: [
+      react(),
+      createHtmlPlugin({
+        minify: true,
+        inject: {
+          data: {
+            title: env.VITE_APP_TITLE || "Local Avocado",
+          },
+        },
+      }),
+    ],
+    base: "/stunning-guacamole/",
+  });
+};
